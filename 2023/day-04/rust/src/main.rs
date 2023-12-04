63f2f58eef0c62c02d80bc324da3955800554207
ttlekich <ttlekich@gmail.com>
2023-12-04 08:24:01 -0600

    day 04 part 2

--- 2023/day-04/rust/src/main.rs
+++ 2023/day-04/rust/src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn part_1() {
@@ -42,6 +43,51 @@ fn part_1() {
         .sum();
     println!("{:?}", sum);
 }
+
+fn part_2() {
+    let file = fs::read_to_string("1.txt").unwrap();
+    let lines = file.lines();
+    let cards: Vec<&str> = lines
+        .map(|line| {
+            let parts: Vec<&str> = line.split(": ").collect();
+            parts[1]
+        })
+        .collect();
+    let mut adds: Vec<(i32, i32)> = cards
+        .iter()
+        .map(|&card| {
+            let parts: Vec<&str> = card.split("| ").collect();
+            parts
+        })
+        .map(|card| {
+            let winning: Vec<i32> = card[0].split_whitespace().map(|n| to_int(n)).collect();
+            let my: Vec<i32> = card[1].split_whitespace().map(|n| to_int(n)).collect();
+            return vec![winning, my];
+        })
+        .map(|card| {
+            let mut matches = 0;
+            let winning = &card[0];
+            let my = &card[1];
+            for n in my.iter() {
+                if winning.contains(n) {
+                    matches = matches + 1
+                }
+            }
+            (1, matches)
+        })
+        .collect();
+    for i in 0..adds.len() {
+        let current = adds[i];
+        for j in 1..=current.1 {
+            let next = adds[i + j as usize];
+            adds[i + j as usize] = (next.0 + current.0, next.1)
+        }
+    }
+    let sum: i32 = adds.iter().map(|t| t.0).sum();
+    println!("{}", sum)
+}
+
+/******************************************************************************/
 
 fn to_int(s: &str) -> i32 {
     s.parse::<i32>().unwrap()
