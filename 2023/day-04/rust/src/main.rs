0797e263216f697a3258bd243c7d1efacf8f9fa0
ttlekich <ttlekich@gmail.com>
2023-12-04 08:01:06 -0600

    day 04 part 1

---
+++ 2023/day-04/rust/src/main.rs
@@ -0,0 +1,48 @@
+use std::fs;
+
+fn main() {
+    part_1();
+}
+
+fn part_1() {
+    let file = fs::read_to_string("1.txt").unwrap();
+    let lines = file.lines();
+    let cards: Vec<&str> = lines
+        .map(|line| {
+            let parts: Vec<&str> = line.split(": ").collect();
+            parts[1]
+        })
+        .collect();
+    let sum: i32 = cards
+        .iter()
+        .map(|&card| {
+            let parts: Vec<&str> = card.split("| ").collect();
+            parts
+        })
+        .map(|card| {
+            let winning: Vec<i32> = card[0].split_whitespace().map(|n| to_int(n)).collect();
+            let my: Vec<i32> = card[1].split_whitespace().map(|n| to_int(n)).collect();
+            return vec![winning, my];
+        })
+        .map(|card| {
+            let mut score = 0;
+            let winning = &card[0];
+            let my = &card[1];
+            for n in my.iter() {
+                if winning.contains(n) {
+                    if score == 0 {
+                        score = 1;
+                        continue;
+                    }
+                    score = score * 2
+                }
+            }
+            score
+        })
+        .sum();
+    println!("{:?}", sum);
+}
+
+fn to_int(s: &str) -> i32 {
+    s.parse::<i32>().unwrap()
+}
