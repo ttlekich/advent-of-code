7cf91f623ca755bc26e8df313b2da902db06e29d
ttlekich <ttlekich@gmail.com>
2023-12-05 12:09:25 -0600

    part 2

--- 2023/day-05/rust/src/main.rs
+++ 2023/day-05/rust/src/main.rs
@@ -1,7 +1,10 @@
 use std::fs;
 
+use rayon::prelude::*;
+
 fn main() {
     part_1();
+    part_2();
 }
 
 fn part_1() {
@@ -11,14 +14,28 @@ fn part_1() {
     let maps: &Vec<Vec<Map>> = &parts[1..].iter().map(|&part| parse_map(&part)).collect();
     let locations: Vec<i64> = seeds
         .iter()
-        .map(|seed| seed_to_location(&seed, maps))
+        .map(|&seed| seed_to_location(seed, maps))
         .collect();
     println!("{:?}", locations.iter().min());
 }
 
-#[derive(Debug)]
-struct Seed {
-    number: i64,
+fn part_2() {
+    let lines = fs::read_to_string("1.txt").unwrap();
+    let parts: Vec<_> = lines.split("\n\n").collect();
+    let seed_ranges = parse_seed_ranges(&parts);
+    let seeds = parse_seeds_2(&seed_ranges);
+    println!("[# Seeds]          : {:?}", seeds.len());
+    let maps: &Vec<Vec<Map>> = &parts[1..].iter().map(|&part| parse_map(&part)).collect();
+    println!("[# Maps]           : {:?}", maps.len());
+    let min_locations = seeds.par_chunks(100000000).map(|chunk| {
+        let min = chunk
+            .par_iter()
+            .map(|&seed| seed_to_location(seed, maps))
+            .min();
+        println!("[# CHUNK DONE]");
+        min
+    });
+    println!("[Min Location]     :{:?}", min_locations.min());
 }
 
 #[derive(Debug)]
@@ -28,7 +45,7 @@ struct Map {
     range_length: i64,
 }
 
-fn parse_seeds(parts: &Vec<&str>) -> Vec<Seed> {
+fn parse_seeds(parts: &Vec<&str>) -> Vec<i64> {
     let raw_seeds = parts[0];
     let raw_seeds_parts: Vec<&str> = raw_seeds.split(": ").collect();
     let raw_seed_numbers = raw_seeds_parts[1];
@@ -36,8 +53,27 @@ fn parse_seeds(parts: &Vec<&str>) -> Vec<Seed> {
         .split_whitespace()
         .map(|seed_number| {
             let number = seed_number.parse::<i64>().unwrap();
-            Seed { number: number }
+            number
         })
+        .collect();
+    seeds
+}
+
+fn parse_seed_ranges(parts: &Vec<&str>) -> Vec<(i64, i64)> {
+    let raw_seeds = parts[0];
+    let raw_seeds_parts: Vec<&str> = raw_seeds.split(": ").collect();
+    let raw_seed_numbers = raw_seeds_parts[1];
+    let ranges: Vec<i64> = raw_seed_numbers
+        .split_whitespace()
+        .map(|raw_number| raw_number.parse::<i64>().unwrap())
+        .collect();
+    ranges.chunks(2).map(|x| (x[0], x[1])).collect()
+}
+
+fn parse_seeds_2(seed_ranges: &Vec<(i64, i64)>) -> Vec<i64> {
+    let seeds = seed_ranges
+        .par_iter()
+        .flat_map(|range| (range.0..(range.0 + range.1)))
         .collect();
     seeds
 }
@@ -59,8 +95,8 @@ fn parse_map(part: &str) -> Vec<Map> {
     maps
 }
 
-fn seed_to_location(seed: &Seed, list_of_maps: &Vec<Vec<Map>>) -> i64 {
-    let mut current_source = seed.number;
+fn seed_to_location(seed: i64, list_of_maps: &Vec<Vec<Map>>) -> i64 {
+    let mut current_source = seed;
     for maps in list_of_maps.iter() {
         let destination = map_source(maps, current_source);
         current_source = destination;
