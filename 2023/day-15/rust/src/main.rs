33841ed79689053b08b71d6f5fe7fe2e052de36f
ttlekich <ttlekich@gmail.com>
2023-12-15 07:59:21 -0800

    part 2

--- 2023/day-15/rust/src/main.rs
+++ 2023/day-15/rust/src/main.rs
@@ -3,7 +3,9 @@ use std::{collections::HashMap, fs};
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     let output_1 = part_1(&input);
-    println!("{:?}", output_1);
+    println!("[PART 1] :: {:?}", output_1);
+    let output_2 = part_2(&input);
+    println!("[PART 2] :: {:?}", output_2);
 }
 
 fn part_1(input: &str) -> i64 {
@@ -26,13 +28,13 @@ fn hash(step: &str) -> i64 {
     current
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Op {
     Equal,
     Minus,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Step {
     label: String,
     hash: i64,
@@ -45,17 +47,47 @@ fn part_2(input: &str) -> i64 {
     let mut boxes: HashMap<i64, Vec<Step>> = HashMap::new();
     for step in steps.iter() {
         match step.op {
-            Op::Equal => {
+            Op::Minus => {
                 if let Some(values) = boxes.get(&step.hash) {
-                    // get new values;
-                    boxes.insert(step.hash, v);
+                    let mut new_values = values.to_vec();
+                    new_values.retain(|x| x.label != step.label);
+                    boxes.insert(step.hash, new_values);
                 }
             }
-            Op::Minus => {}
+            Op::Equal => {
+                if let Some(values) = boxes.get(&step.hash) {
+                    let mut new_values = values.to_vec();
+                    let index_result = new_values.iter().position(|v| v.label == step.label);
+                    if let Some(index) = index_result {
+                        new_values[index] = step.clone();
+                        boxes.insert(step.hash, new_values);
+                    } else {
+                        let mut new_values = values.to_vec();
+                        new_values.push(step.clone());
+                        boxes.insert(step.hash, new_values);
+                    }
+                } else {
+                    boxes.insert(step.hash, vec![step.clone()]);
+                }
+            }
         }
     }
+    let sum = boxes
+        .keys()
+        .map(|k| {
+            let vs = boxes.get(k).unwrap();
+            vs.iter()
+                .enumerate()
+                .map(|(slot_n, v)| calculate_focusing_power(v, k.clone(), (slot_n + 1) as i64))
+                .sum::<i64>()
+        })
+        .sum::<i64>();
 
-    todo!()
+    sum
+}
+
+fn calculate_focusing_power(v: &Step, k: i64, slot_n: i64) -> i64 {
+    (k + 1) * slot_n * v.focal_length
 }
 
 fn parse_steps(input: &str) -> Vec<Step> {
@@ -67,16 +99,20 @@ fn parse_steps(input: &str) -> Vec<Step> {
                 Step {
                     label: parts[0].to_string(),
                     op: Op::Equal,
-                    focal_length: parts[2].parse::<i64>().unwrap(),
+                    focal_length: parts[1].parse::<i64>().unwrap(),
                     hash: hash(parts[0]),
                 }
             }
             x if s.contains('-') => {
-                let parts = x.split("=").collect::<Vec<&str>>();
+                let parts = x.split("-").collect::<Vec<&str>>();
                 Step {
                     label: parts[0].to_string(),
-                    op: Op::Equal,
-                    focal_length: parts[2].parse::<i64>().unwrap(),
+                    op: Op::Minus,
+                    focal_length: if parts[1] != "" {
+                        parts[1].parse::<i64>().unwrap()
+                    } else {
+                        0
+                    },
                     hash: hash(parts[0]),
                 }
             }
