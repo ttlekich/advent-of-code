2235780e84ffa48686f0ba83f1b309071aba199e
ttlekich <ttlekich@gmail.com>
2023-12-16 08:27:35 -0800

    part 2

--- 2023/day-16/rust/src/main.rs
+++ 2023/day-16/rust/src/main.rs
@@ -1,13 +1,14 @@
-use core::borrow;
-use std::{fs, ops::Add};
+use std::{collections::HashSet, fs, ops::Add};
 
 fn main() {
-    let input = fs::read_to_string("test.txt").unwrap();
+    let input = fs::read_to_string("input.txt").unwrap();
     let one = part_1(&input);
     println!("{:?}", one);
+    let two = part_2(&input);
+    println!("{:?}", two);
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Direction {
     Right,
     Left,
@@ -26,7 +27,7 @@ impl Direction {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Vector {
     x: i64,
     y: i64,
@@ -45,40 +46,87 @@ impl Add<Vector> for Vector {
 
 type Board = Vec<Vec<char>>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Move {
     vector: Vector,
     direction: Direction,
 }
 
-fn part_1(input: &str) -> () {
-    let mut board = parse_tiles(&input);
-    let mut stack: Vec<Move> = vec![Move {
-        vector: Vector { x: 0, y: 0 },
-        direction: Direction::Right,
-    }];
+fn part_2(input: &str) -> usize {
+    let board = parse_tiles(&input);
+    let max = (board.len() - 1) as i64;
+    let initial_moves = (0..board.len()).flat_map(|i| {
+        vec![
+            Move {
+                vector: Vector { x: i as i64, y: 0 },
+                direction: Direction::Down,
+            },
+            Move {
+                vector: Vector {
+                    x: i as i64,
+                    y: max,
+                },
+                direction: Direction::Up,
+            },
+            Move {
+                vector: Vector {
+                    x: 0 as i64,
+                    y: i as i64,
+                },
+                direction: Direction::Right,
+            },
+            Move {
+                vector: Vector {
+                    x: max as i64,
+                    y: i as i64,
+                },
+                direction: Direction::Left,
+            },
+        ]
+    });
 
-    // while !stack.is_empty() {
-    //     tick(&mut board, &mut stack, &board);
-    // }
-    for _ in 0..10000 {
-        tick(&mut board, &mut stack);
-    }
+    let solutions = initial_moves.map(|m| solve(&board, m));
 
-    _pretty_print(&board);
-    _pretty_print(&board);
-    println!("{:?}", stack);
-
-    todo!()
+    solutions.max().unwrap()
 }
 
-fn tick(board: &mut Board, stack: &mut Vec<Move>) -> () {
+fn part_1(input: &str) -> usize {
+    let board = parse_tiles(&input);
+    let initial = Move {
+        vector: Vector { x: 0, y: 0 },
+        direction: Direction::Right,
+    };
+
+    solve(&board, initial.clone())
+}
+
+fn solve(board: &Board, initial: Move) -> usize {
+    let mut current_board = board.clone();
+    let mut stack: Vec<Move> = vec![initial];
+    let mut visited: HashSet<Move> = HashSet::new();
+    let mut energized: HashSet<Vector> = HashSet::new();
+
+    while !stack.is_empty() {
+        tick(&mut current_board, &mut stack, &mut visited);
+    }
+
+    for v in visited.iter() {
+        energized.insert(v.vector);
+    }
+
+    energized.len()
+}
+
+fn tick(board: &mut Board, stack: &mut Vec<Move>, visited: &mut HashSet<Move>) -> () {
     if let Some(current_move) = stack.pop() {
         if current_move.vector.y >= board.len() as i64
             || current_move.vector.x >= board[0].len() as i64
             || current_move.vector.y < 0
             || current_move.vector.x < 0
         {
+            return;
+        }
+        if visited.contains(&current_move) {
             return;
         }
         let current_tile = board[current_move.vector.y as usize][current_move.vector.x as usize];
@@ -90,144 +138,133 @@ fn tick(board: &mut Board, stack: &mut Vec<Move>) -> () {
                 });
                 board[current_move.vector.y as usize][current_move.vector.x as usize] = '#';
             }
-            // '#' => stack.push(Move {
-            //     vector: current_move.vector + current_move.direction.value(),
-            //     direction: current_move.direction,
-            // }),
-            '/' => {
-                board[current_move.vector.y as usize][current_move.vector.x as usize] = '#';
-                match current_move.direction {
-                    Direction::Right => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Up.value(),
-                            direction: Direction::Up,
-                        });
-                    }
-                    Direction::Left => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Down.value(),
-                            direction: Direction::Down,
-                        });
-                    }
-                    Direction::Up => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Right.value(),
-                            direction: Direction::Right,
-                        });
-                    }
-                    Direction::Down => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Left.value(),
-                            direction: Direction::Left,
-                        });
-                    }
+            '#' => stack.push(Move {
+                vector: current_move.vector + current_move.direction.value(),
+                direction: current_move.direction,
+            }),
+            '/' => match current_move.direction {
+                Direction::Right => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Up.value(),
+                        direction: Direction::Up,
+                    });
                 }
-            }
-            '\\' => {
-                board[current_move.vector.y as usize][current_move.vector.x as usize] = '#';
-                match current_move.direction {
-                    Direction::Right => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Down.value(),
-                            direction: Direction::Down,
-                        });
-                    }
-                    Direction::Left => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Up.value(),
-                            direction: Direction::Up,
-                        });
-                    }
-                    Direction::Up => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Left.value(),
-                            direction: Direction::Left,
-                        });
-                    }
-                    Direction::Down => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Right.value(),
-                            direction: Direction::Right,
-                        });
-                    }
+                Direction::Left => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Down.value(),
+                        direction: Direction::Down,
+                    });
                 }
-            }
-            '-' => {
-                board[current_move.vector.y as usize][current_move.vector.x as usize] = '#';
-                match current_move.direction {
-                    Direction::Right => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Right.value(),
-                            direction: Direction::Right,
-                        });
-                    }
-                    Direction::Left => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Left.value(),
-                            direction: Direction::Left,
-                        });
-                    }
-                    Direction::Up => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Left.value(),
-                            direction: Direction::Left,
-                        });
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Right.value(),
-                            direction: Direction::Right,
-                        });
-                    }
-                    Direction::Down => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Left.value(),
-                            direction: Direction::Left,
-                        });
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Right.value(),
-                            direction: Direction::Right,
-                        });
-                    }
+                Direction::Up => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Right.value(),
+                        direction: Direction::Right,
+                    });
                 }
-            }
-            '|' => {
-                board[current_move.vector.y as usize][current_move.vector.x as usize] = '#';
-                match current_move.direction {
-                    Direction::Right => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Up.value(),
-                            direction: Direction::Up,
-                        });
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Down.value(),
-                            direction: Direction::Down,
-                        });
-                    }
-                    Direction::Left => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Up.value(),
-                            direction: Direction::Up,
-                        });
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Down.value(),
-                            direction: Direction::Down,
-                        });
-                    }
-                    Direction::Up => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Up.value(),
-                            direction: Direction::Left,
-                        });
-                    }
-                    Direction::Down => {
-                        stack.push(Move {
-                            vector: current_move.vector + Direction::Down.value(),
-                            direction: Direction::Down,
-                        });
-                    }
+                Direction::Down => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Left.value(),
+                        direction: Direction::Left,
+                    });
                 }
-            }
+            },
+            '\\' => match current_move.direction {
+                Direction::Right => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Down.value(),
+                        direction: Direction::Down,
+                    });
+                }
+                Direction::Left => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Up.value(),
+                        direction: Direction::Up,
+                    });
+                }
+                Direction::Up => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Left.value(),
+                        direction: Direction::Left,
+                    });
+                }
+                Direction::Down => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Right.value(),
+                        direction: Direction::Right,
+                    });
+                }
+            },
+            '-' => match current_move.direction {
+                Direction::Right => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Right.value(),
+                        direction: Direction::Right,
+                    });
+                }
+                Direction::Left => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Left.value(),
+                        direction: Direction::Left,
+                    });
+                }
+                Direction::Up => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Left.value(),
+                        direction: Direction::Left,
+                    });
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Right.value(),
+                        direction: Direction::Right,
+                    });
+                }
+                Direction::Down => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Left.value(),
+                        direction: Direction::Left,
+                    });
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Right.value(),
+                        direction: Direction::Right,
+                    });
+                }
+            },
+            '|' => match current_move.direction {
+                Direction::Right => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Up.value(),
+                        direction: Direction::Up,
+                    });
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Down.value(),
+                        direction: Direction::Down,
+                    });
+                }
+                Direction::Left => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Up.value(),
+                        direction: Direction::Up,
+                    });
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Down.value(),
+                        direction: Direction::Down,
+                    });
+                }
+                Direction::Up => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Up.value(),
+                        direction: Direction::Up,
+                    });
+                }
+                Direction::Down => {
+                    stack.push(Move {
+                        vector: current_move.vector + Direction::Down.value(),
+                        direction: Direction::Down,
+                    });
+                }
+            },
             _ => unreachable!(),
         }
+        visited.insert(current_move.clone());
     }
 }
 
