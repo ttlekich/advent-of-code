9d964306343ea8e38366e046bc2fa41e3833a609
ttlekich <ttlekich@gmail.com>
2023-12-19 13:51:50 -0800

    clean up

--- 2023/day-17/rust/src/main.rs
+++ 2023/day-17/rust/src/main.rs
@@ -1,36 +1,10 @@
 use std::{
-    cmp::{Ordering, Reverse},
+    cmp::Reverse,
     collections::{BinaryHeap, HashSet},
     fmt::Display,
     fs,
     ops::Add,
 };
-
-#[derive(Debug, Clone)]
-struct Data {
-    node: Node,
-    cost: Cost,
-}
-
-impl Ord for Data {
-    fn cmp(&self, other: &Self) -> Ordering {
-        ((self.cost, &self.node)).cmp(&(other.cost, &other.node))
-    }
-}
-
-impl PartialOrd for Data {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl PartialEq for Data {
-    fn eq(&self, other: &Self) -> bool {
-        (&self.node, self.cost) == (&other.node, other.cost)
-    }
-}
-
-impl Eq for Data {}
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
@@ -100,7 +74,6 @@ fn part_1(input: &str) -> () {
 }
 
 fn part_2(input: &str) -> () {
-    println!("======");
     let board = parse_tiles(&input);
     let source = Node { x: 0, y: 0 };
     let mut visited = HashSet::new();
@@ -210,9 +183,6 @@ struct Node {
     x: i64,
     y: i64,
 }
-
-type Weight = i64;
-type Cost = i64;
 
 impl Add<Node> for Node {
     type Output = Node;
