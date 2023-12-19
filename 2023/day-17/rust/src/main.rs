8aaa2c4dace6feb40d7ee0d7dcabec34e2604972
ttlekich <ttlekich@gmail.com>
2023-12-19 13:50:51 -0800

    day 17

--- 2023/day-17/rust/src/main.rs
+++ 2023/day-17/rust/src/main.rs
@@ -1,111 +1,167 @@
 use std::{
-    collections::{BinaryHeap, HashMap, HashSet, VecDeque},
+    cmp::{Ordering, Reverse},
+    collections::{BinaryHeap, HashSet},
     fmt::Display,
     fs,
     ops::Add,
 };
 
+#[derive(Debug, Clone)]
+struct Data {
+    node: Node,
+    cost: Cost,
+}
+
+impl Ord for Data {
+    fn cmp(&self, other: &Self) -> Ordering {
+        ((self.cost, &self.node)).cmp(&(other.cost, &other.node))
+    }
+}
+
+impl PartialOrd for Data {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Data {
+    fn eq(&self, other: &Self) -> bool {
+        (&self.node, self.cost) == (&other.node, other.cost)
+    }
+}
+
+impl Eq for Data {}
+
 fn main() {
-    let input = fs::read_to_string("test.txt").unwrap();
+    let input = fs::read_to_string("input.txt").unwrap();
     let one = part_1(&input);
     println!("{:?}", one);
     let two = part_2(&input);
     println!("{:?}", two);
 }
 
-struct Data {
-    cost: i64,
-    count: i64,
-    parent: Move,
-}
-
 fn part_1(input: &str) -> () {
-    let mut board = parse_tiles(&input);
-    let start = Move {
-        direction: Direction::Right,
-        vector: Node { x: 0, y: 0 },
-        weight: board[0][0],
-    };
-    let mut shortest: HashMap<Move, Data> = HashMap::new();
-    let mut visited: HashSet<Move> = HashSet::new();
-    let mut queue = VecDeque::new();
-    queue.push_back((start, 0, 1 as i64));
-    loop {
-        if queue.is_empty() {
+    let board = parse_tiles(&input);
+    let source = Node { x: 0, y: 0 };
+    let mut visited = HashSet::new();
+    let mut pq = BinaryHeap::new();
+    pq.push(Reverse((0, source, Direction::None, 0)));
+
+    while !pq.is_empty() {
+        let Reverse((hl, n1, direction, direction_count)) = pq.pop().unwrap();
+
+        if n1.x == (board[0].len() - 1) as i64 && n1.y == (board.len() - 1) as i64 {
+            println!("{}", hl);
             break;
         }
-        let (m, w, c) = queue.pop_front().unwrap();
-        if let Some(_) = visited.get(&m) {
+
+        if visited.contains(&(n1, direction, direction_count)) {
             continue;
         }
-        println!("n {:?}", c);
-        visited.insert(m.clone());
-        for next_m in adj_moves(&board, &m, c) {
-            let next_w = next_m.weight + w;
-            if let Some(existing_data) = shortest.get(&next_m.clone()) {
-                if next_w >= existing_data.cost {
-                    continue;
+        visited.insert((n1, direction, direction_count));
+
+        if direction_count < 3 && direction != Direction::None {
+            let n2 = n1 + direction.value();
+            if n2.x < board[0].len() as i64 && n2.x >= 0 && n2.y < board.len() as i64 && n2.y >= 0 {
+                pq.push(Reverse((
+                    hl + board[n2.y as usize][n2.x as usize],
+                    n2,
+                    direction,
+                    direction_count + 1,
+                )));
+            }
+        }
+
+        for next_direction in vec![
+            Direction::Right,
+            Direction::Left,
+            Direction::Up,
+            Direction::Down,
+        ]
+        .iter()
+        {
+            if direction != *next_direction && direction.invert() != *next_direction {
+                let n2 = n1 + next_direction.value();
+                if n2.x < board[0].len() as i64
+                    && n2.x >= 0
+                    && n2.y < board.len() as i64
+                    && n2.y >= 0
+                {
+                    pq.push(Reverse((
+                        hl + board[n2.y as usize][n2.x as usize],
+                        n2,
+                        *next_direction,
+                        1,
+                    )));
                 }
             }
-            shortest.insert(next_m.clone());
-            parents.insert(next_m.clone(), m.clone());
-            if next_m.direction == m.direction {
-                queue.push_back((next_m.clone(), next_w, c + 1));
-            } else {
-                queue.push_back((next_m.clone(), next_w, 1));
-            }
         }
     }
-
-    let mut p: Move;
-    for (k, v) in shortest.iter() {
-        if k.vector.x == (board.len() - 1) as i64 && k.vector.y == (board.len() - 1) as i64 {
-            let mut new_board = board.clone();
-            println!("weight {:?}", v);
-            p = k.clone();
-            while let Some(new_p) = parents.get(&p) {
-                new_board[p.vector.y as usize][p.vector.x as usize] = 0;
-                p = new_p.clone();
-            }
-            _pretty_print(&new_board);
-        }
-    }
-
-    // println!("{:?}", shortest);
-    // _pretty_print(&board);
-}
-
-fn adj_moves(board: &Board, m: &Move, c: i64) -> Vec<Move> {
-    let mut result = vec![];
-    let max = board.len() as i64;
-    let directions = [
-        Direction::Up,
-        Direction::Down,
-        Direction::Left,
-        Direction::Right,
-    ];
-    for direction in directions {
-        let v = m.vector + direction.value();
-        if v.x >= 0 && v.x < max && v.y >= 0 && v.y < max {
-            let next = Move {
-                direction: direction,
-                vector: v,
-                weight: board[v.y as usize][v.x as usize],
-            };
-            result.push(next);
-        }
-    }
-    if !(c < 3) {
-        result = result
-            .into_iter()
-            .filter(|r| r.direction != m.direction)
-            .collect::<Vec<Move>>()
-    }
-    result
 }
 
 fn part_2(input: &str) -> () {
-    todo!()
+    println!("======");
+    let board = parse_tiles(&input);
+    let source = Node { x: 0, y: 0 };
+    let mut visited = HashSet::new();
+    let mut pq = BinaryHeap::new();
+    pq.push(Reverse((0, source, Direction::None, 0)));
+
+    while !pq.is_empty() {
+        let Reverse((hl, n1, direction, direction_count)) = pq.pop().unwrap();
+
+        if n1.x == (board[0].len() - 1) as i64
+            && n1.y == (board.len() - 1) as i64
+            && direction_count >= 4
+        {
+            println!("{}", hl);
+            break;
+        }
+
+        if visited.contains(&(n1, direction, direction_count)) {
+            continue;
+        }
+        visited.insert((n1, direction, direction_count));
+
+        if direction_count < 10 && direction != Direction::None {
+            let n2 = n1 + direction.value();
+            if n2.x < board[0].len() as i64 && n2.x >= 0 && n2.y < board.len() as i64 && n2.y >= 0 {
+                pq.push(Reverse((
+                    hl + board[n2.y as usize][n2.x as usize],
+                    n2,
+                    direction,
+                    direction_count + 1,
+                )));
+            }
+        }
+
+        if direction_count >= 4 || direction == Direction::None {
+            for next_direction in vec![
+                Direction::Right,
+                Direction::Left,
+                Direction::Up,
+                Direction::Down,
+            ]
+            .iter()
+            {
+                if direction != *next_direction && direction.invert() != *next_direction {
+                    let n2 = n1 + next_direction.value();
+                    if n2.x < board[0].len() as i64
+                        && n2.x >= 0
+                        && n2.y < board.len() as i64
+                        && n2.y >= 0
+                    {
+                        pq.push(Reverse((
+                            hl + board[n2.y as usize][n2.x as usize],
+                            n2,
+                            *next_direction,
+                            1,
+                        )));
+                    }
+                }
+            }
+        }
+    }
 }
 
 fn parse_tiles(input: &str) -> Board {
@@ -119,12 +175,13 @@ fn parse_tiles(input: &str) -> Board {
         .collect::<Vec<Vec<i64>>>()
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 enum Direction {
     Right,
     Left,
     Up,
     Down,
+    None,
 }
 
 impl Direction {
@@ -134,15 +191,28 @@ impl Direction {
             Direction::Left => Node { x: -1, y: 0 },
             Direction::Up => Node { x: 0, y: -1 },
             Direction::Down => Node { x: 0, y: 1 },
+            Direction::None => Node { x: 0, y: 0 },
+        }
+    }
+    fn invert(&self) -> Direction {
+        match *self {
+            Direction::Right => Direction::Left,
+            Direction::Left => Direction::Right,
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::None => Direction::None,
         }
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 struct Node {
     x: i64,
     y: i64,
 }
+
+type Weight = i64;
+type Cost = i64;
 
 impl Add<Node> for Node {
     type Output = Node;
@@ -156,13 +226,6 @@ impl Add<Node> for Node {
 }
 
 type Board = Vec<Vec<i64>>;
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct Move {
-    vector: Node,
-    direction: Direction,
-    weight: i64,
-}
 
 fn _pretty_print<T: Display>(a: &Vec<Vec<T>>) {
     for row in a.iter() {
